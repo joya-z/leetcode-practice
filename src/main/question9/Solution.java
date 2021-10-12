719db713140db7d0508502109b7a60337f5f9bf2
joya <joya-z@users.noreply.github.com>
2021-10-13 01:06:45 +0800

    2021-10-13

---
+++ src/main/question9/Solution.java
@@ -0,0 +1,21 @@
+package question9;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome(1231));
+    }
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int origin = x;
+        int reverse = 0;
+
+        while (x !=0){
+            reverse = reverse*10 + x%10;
+            x = x/10;
+        }
+        return origin == reverse;
+    }
+}
